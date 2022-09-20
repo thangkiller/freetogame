b983ed0c750fdf3ed920f375ad1ee5ac47b3ac3a
thangkiller <ngenvan000@gmail.com>
2022-09-20 23:06:01 +0700

    search css and sidebar css

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -64,72 +64,107 @@ const tags = [
 function HomePage() {
   return (
     <div className={cx('wrapper')}>
-      <div className={cx('search')}>
-        <div className={cx('search--title')}>
-          <h1>Advanced Game Search</h1>
-          <p>Find and filter free-to-play games your way!</p>
-        </div>
-        <div className={cx('search--content')}>
-          <FontAwesomeIcon icon={faCircleXmark} />
-          <span>Clear Filters</span>
-        </div>
-      </div>
-      <div className={cx('sidebar')}>
-        <div className={cx('sidebar--sort')}>
-          <div className={cx('sidebar--sort--title')}>
-            <button>Sort By:</button>
-            <FontAwesomeIcon icon={faUpDown} />
+      <div className={cx('grid')}>
+        <div className={cx('search')}>
+          <div className={cx('search--title')}>
+            <h1>Advanced Game Search</h1>
+            <p>Find and filter free-to-play games your way!</p>
           </div>
-          <div className={cx('sidebar--sort--select')}>
-            <div className={cx('sidebar--sort--select--title')}>Platform</div>
-            <ul>
-              <li>PC</li>
-              <li>Browser</li>
-            </ul>
+          <div className={cx('search--content')}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+            <span>Clear Filters</span>
           </div>
         </div>
-        <div className={cx('sidebar--filter')}>
-          <div className={cx('sidebar--filter--title')}>
-            <button>Filter Type:</button>
-            <FontAwesomeIcon icon={faUpDown} />
-          </div>
-          <div className={cx('sidebar--filter--select')}>
-            <div className={cx('sidebar--filter--select--title')}>Genre</div>
-            <ul>
-              {genres.map((genre, index) => {
-                return <li key={index}>{genre}</li>;
-              })}
-            </ul>
-          </div>
-        </div>
-        <div className={cx('sidebar--catetory')}>
-          {catetory.map((item, index) => {
-            return (
-              <div className={cx('sidebar--catetory--select')}>
-                <div className={cx('sidebar--catetory--select--title')}>
-                  {item.name}
-                </div>
-                <ul>
-                  {item.types.map((type, index) => {
-                    return <li key={index}>{type}</li>;
-                  })}
-                </ul>
+        <div className={cx('sidebar')}>
+          <div className={cx('sidebar--group')}>
+            <div className={cx('sidebar--group--title')}>
+              <button>Sort By:</button>
+              <FontAwesomeIcon icon={faUpDown} />
+            </div>
+            <div className={cx('sidebar--group--select')}>
+              <div className={cx('sidebar--group--select--title')}>
+                Platform
               </div>
-            );
-          })}
-        </div>
-        <div className={cx('sidebar--tag')}>
-          <div className={cx('sidebar--tag--select')}>
-            <div className={cx('sidebar--tag--select--title')}>Tags</div>
-            <ul>
-              {tags.map((tag, index) => {
-                return <li key={index}>{tag}</li>;
-              })}
-            </ul>
+              <ul>
+                <li>
+                  <input type="checkbox" />
+                  <span>PC</span>
+                </li>
+                <li>
+                  <input type="checkbox" />
+                  <span>Browser</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className={cx('sidebar--group')}>
+            <div className={cx('sidebar--group--title')}>
+              <button>Filter Type:</button>
+              <FontAwesomeIcon icon={faUpDown} />
+            </div>
+            <div
+              className={cx(
+                'sidebar--group--select',
+                'sidebar--group--select__scroll'
+              )}
+            >
+              <div className={cx('sidebar--group--select--title')}>Genre</div>
+              <ul>
+                {genres.map((genre, index) => {
+                  return (
+                    <li key={index}>
+                      <input type="checkbox" />
+                      <span>{genre}</span>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+          <div className={cx('sidebar--group')}>
+            {catetory.map((item, index) => {
+              return (
+                <div className={cx('sidebar--group--select')}>
+                  <div className={cx('sidebar--group--select--title')}>
+                    {item.name}
+                  </div>
+                  <ul>
+                    {item.types.map((type, index) => {
+                      return (
+                        <li key={index}>
+                          <input type="checkbox" />
+                          <span>{type}</span>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              );
+            })}
+          </div>
+          <div className={cx('sidebar--group')}>
+            <div
+              className={cx(
+                'sidebar--group--select',
+                'sidebar--group--select__scroll'
+              )}
+            >
+              <div className={cx('sidebar--group--select--title')}>Tags</div>
+              <ul>
+                {tags.map((tag, index) => {
+                  return (
+                    <li key={index}>
+                      <input type="checkbox" />
+                      <span>{tag}</span>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
           </div>
         </div>
+        <div className={cx('main')}></div>
       </div>
-      <div className={cx('main')}></div>
     </div>
   );
 }
